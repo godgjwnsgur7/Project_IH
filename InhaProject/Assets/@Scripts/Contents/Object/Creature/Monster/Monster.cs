bf87a4553105da7aee8210356ce2809df0b9a507
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-08-13 12:02:39 +0900

    [준혁] 어택 기능 추가 중...
    - 어택 기능 추가 중
    - 테스트 몬스터
    - 폴더 정리

--- InhaProject/Assets/@Scripts/Contents/Object/Creature/Monster/Monster.cs
+++ InhaProject/Assets/@Scripts/Contents/Object/Creature/Monster/Monster.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static Define;
 
-public class Monster : Creature
+public class Monster : Creature, IHitEvent
 {
     // 임시 데이터들
     protected float MoveSpeed = 1;
---
+++ InhaProject/Assets/@Scripts/Contents/Object/Creature/Player/BasePlayerWeapon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHitEvent
+{
+    
+}
+
+public class BasePlayerWeapon : InitBase
+{
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+
+
+        return true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<IHitEvent>() != null)
+        {
+            Debug.Log($"공격 물체 감지 : {other}");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
