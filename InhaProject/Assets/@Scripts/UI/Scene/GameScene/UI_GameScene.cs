8598c56af41e955bd56b863916791277d16a69c4
semin1012 <semin1012@gmail.com>
2024-08-24 17:26:18 +0900

    [세민] 데미지 추가 중이지만 현재 오류 있음 주말동안 수정 예정

--- InhaProject/Assets/@Scripts/UI/Scene/GameScene/UI_GameScene.cs
+++ InhaProject/Assets/@Scripts/UI/Scene/GameScene/UI_GameScene.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class UI_GameScene : UI_BaseScene
 {
     [SerializeField] UI_SkillBar uiSkillBar;
+	[SerializeField] UI_HPBar uiHealthBar;
 
-    public override bool Init()
+	public override bool Init()
     {
         if (base.Init() == false)
             return false;
 
         return true;
-    }
+	}
 }
